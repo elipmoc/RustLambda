8edee3694ead70cfc5bf933d79609d2890a54722
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-06-03 05:06:49 +0900

    SKiASTの表示関数を実装

--- lambda/ski_ast.rs
+++ lambda/ski_ast.rs
@@ -6,3 +6,26 @@ pub enum SkiAST {
     I,
     Apply(Box<SkiAST>, Box<SkiAST>),
 }
+
+
+impl SkiAST {
+    //ASTが表しているラムダ計算の文字列を返す
+    pub fn show(&self) -> String {
+        match self {
+            SkiAST::Apply(left, right) => show_apply(&left, &right),
+            SkiAST::S => "S".to_string(),
+            SkiAST::K => "K".to_string(),
+            SkiAST::I => "I".to_string(),
+        }
+    }
+}
+
+fn show_apply(left: &Box<SkiAST>, right: &Box<SkiAST>) -> String {
+    let left_show = left.show();
+    let right_show = match **right {
+        SkiAST::Apply(_, _) => "(".to_string() + &right.show() + ")",
+        _ => right.show(),
+    };
+    left_show + " " + &right_show
+}
+
